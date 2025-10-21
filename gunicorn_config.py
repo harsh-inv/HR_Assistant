e1541f2b52a138d4aeb9630f6aaa330adacc8595
harsh-inv <harsh.singh@invenio-solutions.com>
2025-10-21 17:46:41 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,8 @@
 import multiprocessing
-
 # Gunicorn config
 bind = "0.0.0.0:10000"
-workers = multiprocessing.cpu_count() * 2 + 1
-timeout = 300  # 5 minutes (increase from default 30 seconds)
+workers = 1  # CHANGE THIS - Force single worker for now
+timeout = 300
 keepalive = 120
 worker_class = "sync"
 max_requests = 1000
